68bb6c766f2578524e0351cc15b7da0b85bdf682
patohdzz <ppato732@gmail.com>
2026-07-22 21:16:02 -0500

    added javascript and updated index file

---
+++ js/script.js
@@ -0,0 +1,18 @@
+console.log("JavaScript file is connected.");
+console.log("Welcome to week 5!");
+
+// single or double quotes are fine for now
+const studentName = "Patricio"; // string type
+let currentWeek = 5;
+// studentName = "kayla"; // cant do this
+
+console.log(studentName);
+console.log(currentWeek);
+
+const secretPassword = "123";
+const birthMonth = "October";
+let hairColor = "brown";
+let shoeSize = 11;
+let hasSiblings = true;
+
+console.log(hasSiblings);
